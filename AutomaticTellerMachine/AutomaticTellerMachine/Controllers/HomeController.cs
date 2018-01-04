2d96df6057b058d4a7f714fd3db4f485d12e10e3
Harsh Varshney <harsh.varshney@cleartax.blr.in>
2018-01-04 12:40:02 +0530

    action names and forms

---
+++ AutomaticTellerMachine/AutomaticTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomaticTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(); //when view is called without parameters, its going to look for the view whose name matches the class method name
+            //and since in the homecontroller, will look for home in the views for the cshtml file
+        }
+
+        //GET /home/about
+        [ActionName("about-this-atm")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having trouble? Send us a message.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.TheMessage = "Thanks, we got your message";
+
+            return View();
+        }
+
+        public ActionResult Foo()
+        {
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVCATM1";
+            if (letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            //return Content(serial);
+            //return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+            return RedirectToAction("Index");
+        }
+    }
+}
